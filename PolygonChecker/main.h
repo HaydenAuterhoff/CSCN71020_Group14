537bbde2e936a069ca9926a2e67f4bde386559cf
Dante262 <91082292+Dante262@users.noreply.github.com>
2021-11-14 20:20:48 -0500

    Starting GetRectangleAngles from scratch

--- PolygonChecker/main.h
+++ PolygonChecker/main.h
@@ -4,3 +4,8 @@ int printShapeMenu();
 int* getTriangleSides(int* triangleSides);
 bool verifyTriangle(float side1, float side2, float side3);
 float* getTriangleAngles(float, float, float);
+float* getRectangleAngles(float, float, float, float);
+float* makeShape1(float, float, float, float);
+float* makeShape2(float, float, float, float);
+float* makeShape3(float, float, float, float);
+float* makeShape4(float, float, float, float);
